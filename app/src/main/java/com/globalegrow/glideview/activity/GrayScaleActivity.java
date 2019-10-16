e5a34babd764a154ed040cd2958f7692da8dab75
dingpeihua <dingpeihua@globalegrow.com>
2019-10-16 11:03:19 +0800

    1、升级1.0.19

--- app/src/main/java/com/globalegrow/glideview/activity/GrayScaleActivity.java
+++ app/src/main/java/com/globalegrow/glideview/activity/GrayScaleActivity.java
@@ -1,13 +1,17 @@
 package com.globalegrow.glideview.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-
-import com.fz.imageloader.widget.RatioImageView;
-import com.globalegrow.glideview.Constants;
-import com.globalegrow.glideview.R;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.fz.imageloader.glide.LoaderListener;
+import com.fz.imageloader.utils.ImageUtils;
+import com.fz.imageloader.widget.RatioImageView;
+import com.globalegrow.glideview.Constants;
+import com.globalegrow.glideview.R;
 
 /**
  * 灰度变换
@@ -24,10 +28,26 @@ public class GrayScaleActivity extends AppCompatActivity {
         int screenWidth = getResources().getDisplayMetrics().widthPixels;
         int newHeight = Math.round(screenWidth * 433f / 650f);
         RatioImageView imageView1 = findViewById(R.id.riv_image_1);
-        imageView1.setImageUrl(Constants.urls[0]/*,screenWidth,newHeight*/);
+        imageView1.setImageUrl(Constants.urls[0], screenWidth, newHeight);
         RatioImageView imageView2 = findViewById(R.id.riv_image_2);
         imageView2.setImageUrl(Constants.urls[1]);
-        RatioImageView imageView3 = findViewById(R.id.riv_image_3);
+        final RatioImageView imageView3 = findViewById(R.id.riv_image_3);
+//        imageView3.setCornerType(RoundedCornersTransformation.CornerType.TOP_LEFT);
+//        imageView3.setRoundedRadius(24);
+        imageView3.setListener(new LoaderListener<BitmapDrawable>() {
+            @Override
+            public boolean onSuccess(BitmapDrawable bitmapDrawable, int width, int height) {
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                bitmap = ImageUtils.toRoundCorner(bitmap, width, height, 40, ImageUtils.CORNER_TOP_LEFT|ImageUtils.CORNER_TOP_RIGHT);
+                imageView3.setImageDrawable(new BitmapDrawable(getResources(), bitmap));
+                return true;
+            }
+
+            @Override
+            public boolean onError(Exception e) {
+                return false;
+            }
+        });
         imageView3.setImageUrl(Constants.urls[2]);
         RatioImageView imageView4 = findViewById(R.id.riv_image_4);
         imageView4.setImageUrl(Constants.urls[3]);
